a052846d1c25f6df6ae020d059ef8deaf7d367e5
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-06-22 06:16:39 +0000

    Done.

--- index.js
+++ index.js
@@ -10,10 +10,49 @@ function dwarfRollCall(dwarves) {
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  
+  let calls = []
+  
+  for(let i = 0; i<planeteerCalls.length; i++){
+    
+    let yelled = (planeteerCalls[i].toUpperCase()+"!")
+    calls.push(yelled)
+    
+  }
+  
+  return calls
+  
 }
 
 function longPlaneteerCalls(words) {
+  
+  let long = false
+  
+  for(let i=0; i<words.length; i++){
+    
+    if(words[i].length>4){
+      long = true
+    }
+    
+  }
+  return long 
 }
 
 function findTheCheese (foods) {
+  
+  for(let i=0; i<foods.length; i++){
+    if(foods[i] =="cheddar"){
+      return "cheddar"
+    }
+    else if (foods[i] =="gouda"){
+      return "gouda"
+    }
+    else if(foods[i] =="camembert"){
+      return "camembert"
+    }
+  
+  }
+  
+  return "no cheese!"
+  
 }
